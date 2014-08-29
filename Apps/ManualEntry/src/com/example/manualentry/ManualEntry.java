0dca34a6322095bb71962020c4bd8e3f686fea68
Bobby <bobbydonald25@gmail.com>
2014-08-29 00:14:43 -0400

    very nice layout for manual entry datapoints

--- Apps/ManualEntry/src/com/example/manualentry/ManualEntry.java
+++ Apps/ManualEntry/src/com/example/manualentry/ManualEntry.java
@@ -2,9 +2,7 @@ package com.example.manualentry;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-
 import edu.uml.cs.isense.comm.API;
 import edu.uml.cs.isense.credentials.CredentialManager;
 import edu.uml.cs.isense.objects.RProjectField;
@@ -15,14 +13,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.format.Time;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -30,8 +33,10 @@ public class ManualEntry extends Activity {
 	public static Context mContext;
 	private API api; 
 	private Boolean useDev = false;
-	private LinearLayout fieldsLayout;
-	ArrayList<RProjectField> fields;
+	private LinearLayout datapointsLayout;
+	private ArrayList<RProjectField> fields;
+	private Button addField;
+	private int datapoints = 0;
 	
 	public static final int LOGIN_STATUS_REQUESTED = 6005;
 	public static final int PROJECT_REQUESTED = 6005;
@@ -45,13 +50,24 @@ public class ManualEntry extends Activity {
 		api.useDev(useDev);
 
 		CredentialManager.login(mContext, api);
+		addField = (Button) findViewById(R.id.adddatapoint);
 		
-		fieldsLayout = (LinearLayout) findViewById(R.id.fields_sv);
-		
-		new addNewFieldsTask().execute();
+		datapointsLayout = (LinearLayout) findViewById(R.id.datapoints_sv);
+		if (fields == null) {
+			new getNewFieldsTask().execute();
+		} else {
+			addFields();
+		}
 
+		addField.setOnClickListener(new OnClickListener(){
 
-		
+			@Override
+			public void onClick(View v) {
+				Log.e("Here", "push button");
+				addFields();				
+			}
+			
+		});
 	}
 
 	@Override
@@ -89,13 +105,102 @@ public class ManualEntry extends Activity {
 				Log.e("MANUAL", "here2");
 
 				clearFields();
-				new addNewFieldsTask().execute();
+				new getNewFieldsTask().execute();
 			}
 		}
 	}
 	
 	private void clearFields() {
-		fieldsLayout.removeAllViews();
+		datapoints = 0;
+		datapointsLayout.removeAllViews();
+	}
+	
+	private void addFields() {
+		datapoints++;	
+
+		LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LinearLayout datapoint = new LinearLayout(mContext);
+		FrameLayout dataPointFrame = new FrameLayout(mContext);
+		
+		dataPointFrame = (FrameLayout) getLayoutInflater().inflate(R.layout.data_point, null);
+		
+		datapoint = (LinearLayout) dataPointFrame.findViewById(R.id.ll_data_point);
+		
+		TextView datapointnumber = new TextView(mContext);
+		datapointnumber.setText("Data Point: " + datapoints);
+		datapointnumber.setWidth(LayoutParams.MATCH_PARENT);
+		
+		datapoint.addView(datapointnumber, 0);	
+		
+		
+		for(int i=0; i<fields.size(); i++) {
+			RProjectField field = fields.get(i);
+			
+			if (field.type == RProjectField.TYPE_TIMESTAMP) {
+				ViewGroup singlefield = new RelativeLayout(mContext);
+				inflater.inflate(R.layout.field, singlefield);
+				
+				EditText et = (EditText) singlefield.findViewById(R.id.field_et);
+				TextView tv = (TextView) singlefield.findViewById(R.id.field_tv);
+				
+
+				SimpleDateFormat sdf = new SimpleDateFormat("HH:mm MM/dd/yyyy");
+				String currentDateandTime = sdf.format(new Date());
+
+				et.setText(currentDateandTime);
+				et.setEnabled(false);
+				et.setInputType(InputType.TYPE_CLASS_DATETIME);
+				tv.setText(field.name + ":");
+				datapoint.addView(singlefield, i+1);	
+
+			} else if (field.type == RProjectField.TYPE_TEXT) {
+				ViewGroup singlefield = new RelativeLayout(mContext);
+				inflater.inflate(R.layout.field, singlefield);
+				
+				EditText et = (EditText) singlefield.findViewById(R.id.field_et);
+				TextView tv = (TextView) singlefield.findViewById(R.id.field_tv);
+				
+				tv.setText(field.name + ":");
+				datapoint.addView(singlefield, i+1);	
+				
+			} else if (field.type == RProjectField.TYPE_NUMBER) {
+				ViewGroup singlefield = new RelativeLayout(mContext);
+				inflater.inflate(R.layout.field, singlefield);
+				
+				EditText et = (EditText) singlefield.findViewById(R.id.field_et);
+				TextView tv = (TextView) singlefield.findViewById(R.id.field_tv);
+				
+				et.setInputType(InputType.TYPE_CLASS_NUMBER);
+				tv.setText(field.name + ":");
+				datapoint.addView(singlefield, i+1);	
+				
+			} else if (field.type == RProjectField.TYPE_LON) {
+				ViewGroup singlefield = new RelativeLayout(mContext);
+				inflater.inflate(R.layout.field, singlefield);
+				
+				EditText et = (EditText) singlefield.findViewById(R.id.field_et);
+				TextView tv = (TextView) singlefield.findViewById(R.id.field_tv);
+				
+				et.setEnabled(false);
+				tv.setText(field.name + ":");
+				datapoint.addView(singlefield, i+1);	
+				
+			} else if (field.type == RProjectField.TYPE_LAT) {
+				ViewGroup singlefield = new RelativeLayout(mContext);
+				inflater.inflate(R.layout.field, singlefield);
+				
+				EditText et = (EditText) singlefield.findViewById(R.id.field_et);
+				TextView tv = (TextView) singlefield.findViewById(R.id.field_tv);
+				
+				et.setEnabled(false);
+				tv.setText(field.name + ":");
+				datapoint.addView(singlefield, i+1);	
+			}
+		}
+		
+		
+		datapointsLayout.addView(dataPointFrame, datapoints - 1);
+		
 	}
 	
 	/**
@@ -103,7 +208,7 @@ public class ManualEntry extends Activity {
 	 * 
 	 * @author Bobby
 	 */
-	public class addNewFieldsTask extends AsyncTask<Void, Integer, Void> {
+	public class getNewFieldsTask extends AsyncTask<Void, Integer, Void> {
 
 
 		/**
@@ -128,78 +233,7 @@ public class ManualEntry extends Activity {
 		@Override
 		protected void onPostExecute(Void result) {
 			super.onPostExecute(result);
-			
-			
-			for(int i=0; i<fields.size(); i++) {
-				Log.e("MANUAL", "here");
-				
-				//TODO add edit texts for fields
-				RProjectField field = fields.get(i);
-				if (field.type == RProjectField.TYPE_TIMESTAMP) {
-					LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					ViewGroup parent = new RelativeLayout(mContext);
-					inflater.inflate(R.layout.field, parent);
-					
-					EditText et = (EditText) parent.findViewById(R.id.field_et);
-					TextView tv = (TextView) parent.findViewById(R.id.field_tv);
-					
-
-					SimpleDateFormat sdf = new SimpleDateFormat("HH:mm MM/dd/yyyy");
-					String currentDateandTime = sdf.format(new Date());
-
-					et.setText(currentDateandTime);
-					et.setEnabled(false);
-					tv.setText(field.name + ":");
-					fieldsLayout.addView(parent, i);	
-
-				} else if (field.type == RProjectField.TYPE_TEXT) {
-					LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					ViewGroup parent = new RelativeLayout(mContext);
-					inflater.inflate(R.layout.field, parent);
-					
-					EditText et = (EditText) parent.findViewById(R.id.field_et);
-					TextView tv = (TextView) parent.findViewById(R.id.field_tv);
-					
-					tv.setText(field.name + ":");
-					fieldsLayout.addView(parent, i);	
-					
-				} else if (field.type == RProjectField.TYPE_NUMBER) {
-					LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					ViewGroup parent = new RelativeLayout(mContext);
-					inflater.inflate(R.layout.field, parent);
-					
-					EditText et = (EditText) parent.findViewById(R.id.field_et);
-					TextView tv = (TextView) parent.findViewById(R.id.field_tv);
-					
-					tv.setText(field.name + ":");
-					fieldsLayout.addView(parent, i);	
-					
-				} else if (field.type == RProjectField.TYPE_LON) {
-					LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					ViewGroup parent = new RelativeLayout(mContext);
-					inflater.inflate(R.layout.field, parent);
-					
-					EditText et = (EditText) parent.findViewById(R.id.field_et);
-					TextView tv = (TextView) parent.findViewById(R.id.field_tv);
-					
-					et.setEnabled(false);
-					tv.setText(field.name + ":");
-					fieldsLayout.addView(parent, i);	
-					
-				} else if (field.type == RProjectField.TYPE_LAT) {
-					LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-					ViewGroup parent = new RelativeLayout(mContext);
-					inflater.inflate(R.layout.field, parent);
-					
-					EditText et = (EditText) parent.findViewById(R.id.field_et);
-					TextView tv = (TextView) parent.findViewById(R.id.field_tv);
-					
-					et.setEnabled(false);
-					tv.setText(field.name + ":");
-					fieldsLayout.addView(parent, i);	
-
-				}
-			}			
+			addFields();				
 		}
 
 	}

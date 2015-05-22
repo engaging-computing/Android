5277ad432cca80173ba70d7c81d401262eda8fbd
Bobby <bobbydonald25@gmail.com>
2015-05-22 12:48:59 -0400

    indentation

--- Apps/Pictures/src/edu/uml/cs/isense/riverwalk/Continuous.java
+++ Apps/Pictures/src/edu/uml/cs/isense/riverwalk/Continuous.java
@@ -13,35 +13,35 @@ public class Continuous extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.continuous_shooting);
-    getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.continuous_shooting);
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
 
-	if (android.os.Build.VERSION.SDK_INT >= 11)
-	setFinishOnTouchOutside(false);
+        if (android.os.Build.VERSION.SDK_INT >= 11)
+        setFinishOnTouchOutside(false);
 
-	final CheckBox continuous_cb = (CheckBox) findViewById(R.id.checkContinuous);
-	continuous_cb.setChecked(Pictures.continuous); // if true then continuous_cb will be set to checked if not then it will not be checked
+        final CheckBox continuous_cb = (CheckBox) findViewById(R.id.checkContinuous);
+        continuous_cb.setChecked(Pictures.continuous); // if true then continuous_cb will be set to checked if not then it will not be checked
 
-	final EditText continuous_time = (EditText) findViewById(R.id.etInterval);
-	continuous_time.setText(String.valueOf(Pictures.continuousInterval));
+        final EditText continuous_time = (EditText) findViewById(R.id.etInterval);
+        continuous_time.setText(String.valueOf(Pictures.continuousInterval));
 
-	continuous_cb.setOnClickListener(new View.OnClickListener() {
+        continuous_cb.setOnClickListener(new View.OnClickListener() {
 
-		@Override
-		public void onClick(View v) {
-			if (continuous_cb.isChecked()){							//Continuous checkbox is checked
-				Pictures.continuous = true;
-				Pictures.addPicture.setVisibility(View.GONE);
-				Pictures.takePicture.setText(R.string.takePicContinuous);
-			}else{													//Continuous checkbox is not checked
-				Pictures.continuous = false;
-				Pictures.addPicture.setVisibility(View.VISIBLE);
-				Pictures.takePicture.setText(R.string.takePicSingle);
-			}
-		}
-	});
+            @Override
+            public void onClick(View v) {
+                if (continuous_cb.isChecked()){							//Continuous checkbox is checked
+                    Pictures.continuous = true;
+                    Pictures.addPicture.setVisibility(View.GONE);
+                    Pictures.takePicture.setText(R.string.takePicContinuous);
+                }else{													//Continuous checkbox is not checked
+                    Pictures.continuous = false;
+                    Pictures.addPicture.setVisibility(View.VISIBLE);
+                    Pictures.takePicture.setText(R.string.takePicSingle);
+                }
+            }
+        });
 
 	final Button ok = (Button) findViewById(R.id.description_okay);
 	ok.setOnClickListener(new OnClickListener() {

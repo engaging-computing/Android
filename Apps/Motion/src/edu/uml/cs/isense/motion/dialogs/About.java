9829114c60c2e2955a751bc68dcdcf56ff8ad649
Bobby <bobbydonald25@gmail.com>
2015-05-22 13:01:30 -0400

    indentation

--- Apps/Motion/src/edu/uml/cs/isense/motion/dialogs/About.java
+++ Apps/Motion/src/edu/uml/cs/isense/motion/dialogs/About.java
@@ -8,19 +8,17 @@ import edu.uml.cs.isense.motion.R;
 
 public class About extends ActionBarActivity {
 
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
         setContentView(R.layout.about);
 
         Button okButton = (Button) findViewById(R.id.okButton);
         okButton.setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				finish();
-			}
-		});
-	}
-
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
